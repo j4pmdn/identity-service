a6919ff88b85f20008d23a148768fc1f9ad3b196
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-04-04 10:10:06 +0700

    advance handling exception

---
+++ src/main/java/com/devteria/identityservice/exception/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.devteria.identityservice.exception;
+
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(999,"Uncategorized exception"),
+    INVALID_KEY(1001,"Uncategorized exception"),
+    USER_EXISTED(1002,"User existed"),
+    USERNAME_INVALID(1003, "Username must be at least 3 charactes"),
+    INVALID_PASSWORD(1004, "Password must be at least 8 charactes")
+    ;
+    private int code;
+    private String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
